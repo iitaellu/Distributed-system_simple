cea175dc6ec984ef239361a1ac1f43a6c6beb486
iitaellu <78799868+iitaellu@users.noreply.github.com>
2023-04-08 14:54:55 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,10 @@
+"""
+https://www.youtube.com/watch?v=F_JDA96AdEI&t=254s
+https://www.neuralnine.com/tcp-chat-in-python/
+https://www.youtube.com/watch?v=3UOyky9sEQY
+
+"""
+
 import threading
 import socket
 
